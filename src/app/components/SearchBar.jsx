0bd7b4c050177e1021a448db72b04d7b96339c13
PabloJL <jup_jim_7@hotmail.com>
2024-06-05 13:24:56 -0600

    ?

---
+++ src/app/components/SearchBar.jsx
@@ -0,0 +1,8 @@
+export default function SearchBar() {
+  return (
+    <div className="search-bar">
+      <input type="text" name="seach" placeholder="Search Location ..." />
+      <button>Search</button>
+    </div>
+  );
+}
